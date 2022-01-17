a5baf034a48977004b6c71c7e21fc3ddb5c9cd49
dathtvq123456789 <73975043+dathtvq123456789@users.noreply.github.com>
2022-01-17 16:55:01 +0700

    bai2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, JavaOOP!");
+        // doi tuong de nhap xuat
+        Scanner sc = new Scanner(System.in);
+        //khai bao bien de chua gia tri nhap vao
+        String strHT = "";
+        //thong bao dau vao
+        System.out.println("Nhap ho ten: ");
+        // gan gia tri tu ban phim
+        strHT = sc.nextLine();
+        System.out.println("Xin chao " + strHT);
     }
 }
